ff173e1679f3d3eb89e027231706cc76bcfcf6f6
Romik271 <roman.bashmakov.de@gmail.com>
2026-07-12 01:00:12 +0200

    images optimized again

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,18 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://www.groundedtouch.de',
   integrations: [tailwind(), sitemap()],
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name && assetInfo.name.endsWith('.css')) {
+              return '_astro/styles.[hash][extname]';
+            }
+            return '_astro/[name].[hash][extname]';
+          },
+        },
+      },
+    },
+  },
 });
